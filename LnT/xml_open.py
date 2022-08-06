22c4b45f52ccdebaa4ad0df6c05d8d4ea8aeee94
Sriram <65888303+Sriram-2210@users.noreply.github.com>
2022-08-06 17:12:39 +0530

    Delete LnT directory

--- LnT/xml_open.py
+++
@@ -1,9 +0,0 @@
-import lxml
-from lxml import etree
-xml_list = []
-with open('Lnt_Project/ACORD_XML_For_SubmittionToCarrier.xml', 'r') as f:
-    root = etree.parse(f)
-    for e in root.iter():
-        path = root.getelementpath(e)
-        print(path)
-
